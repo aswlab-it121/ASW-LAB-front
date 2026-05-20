ed4367db821d414b5c3d59713c8875999c6425e0
GenisLopez5 <misterproxx@gmail.com>
2026-05-20 21:06:20 +0200

    404

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { AppProviders } from './app/providers/AppProviders'
@@ -8,9 +8,9 @@ import { AppProviders } from './app/providers/AppProviders'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProviders>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </AppProviders>
   </React.StrictMode>
 )
